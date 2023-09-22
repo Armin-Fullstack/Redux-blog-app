3e0912fb106a9d7f65ea17d69cabc1d4fb828dd6
Armin Hossein Zadeh <Armin.Fullstack@gmail.com>
2023-09-23 00:48:55 +0330

    feat: created a Reusable <Button /> component

---
+++ src/components/Button.tsx
@@ -0,0 +1,6 @@
+import { ButtonProps } from "../Types"
+import styles from "./Button.module.css"
+
+export default function Button({children , textColor , bgColor, textSize , width}: ButtonProps): JSX.Element {
+  return <button className={styles.btn} style={{color: textColor, backgroundColor: bgColor, fontSize: textSize , width}} >{children}</button>
+}

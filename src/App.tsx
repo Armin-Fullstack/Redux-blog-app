bcaf5f14ed81d13e8ca19bc8ec76195167c31422
Armin Hossein Zadeh <Armin.Fullstack@gmail.com>
2023-09-23 11:57:51 +0330

    feat: implemented Routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 
 export default function App(): JSX.Element {
   return (
+    <>
     <Header />
+    <BrowserRouter>
+      <Routes>
+          <Route path="/" element={<p>main App</p>}/>
+          <Route path="users" element={<p>Users</p>}/>
+          <Route path="notifications" element={<p>notifications</p>}/>
+          <Route path="posts" element={<p>Posts</p>}/>
+      </Routes>
+    </BrowserRouter>
+    </>
   )
 }
